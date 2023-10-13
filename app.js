62c1ee09f8ec4172c952f5030403d5956400023b
Michał Polak <michalpl618@gmail.com>
2023-10-13 13:04:55 +0200

    Zadanie_1

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const _ = require('lodash');
 
 const average = (arr) => {
-    console.log(_.mean(arr));
+    console.log("Srednia [" + arr + "] wynosi: "+ _.mean(arr));
 }
 
 average([1,2,2,3,3,4,9,8,5,4,5,2,1,]);
